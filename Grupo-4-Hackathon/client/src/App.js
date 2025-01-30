cc35f8d47d4360727476ec26f48696e9a35114f5
Xeno <elton.sanches.731@gmail.com>
2025-01-30 01:50:46 +0000

    Navbar, Login
    Co-Authored-By: LucaGCappello <152432062+LucaGCappello@users.noreply.github.com>
    Co-Authored-By: Ali X <185490368+alinaiscruz@users.noreply.github.com>

--- Grupo-4-Hackathon/client/src/App.js
+++ Grupo-4-Hackathon/client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FormPage from '../src/components/FormPage/FormPage'; 
 import HomePage from '../src/components/HomePage/HomePage'; 
 import ContactPage from '../src/components/ContactPage/ContactPage';
+import { Login } from './components/LoginPage/LoginPage';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/formulario" element={<FormPage />} />
         <Route path="/contato" element={<ContactPage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
